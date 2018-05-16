18dc1cb2e375d14a0bd94b0c08bcb620d20b0380
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-05-16 13:30:24 +0900

    install django package

--- test/integration/default/install_packages_test.rb
+++ test/integration/default/install_packages_test.rb
@@ -1,6 +1,9 @@
 # # encoding: utf-8
 
-# Inspec test for recipe django::default
+# Inspec test for recipe django::install_packages.rb
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+describe command 'django-admin --version' do
+  its(:stdout) { should match(/1.8.7/) }
+end
